04c008ca97b61453fb9f1219926f9472011e65d7
Mginx <max@ginter-wersant.lu>
2016-04-09 02:19:51 +0200

    finished checkNeighbour function

--- js/checkNeighbours.js
+++ js/checkNeighbours.js
@@ -1,3 +1,21 @@
 function checkNeighbours(x,y){
-	
+	if (field[x-1][y-1].exists===true) {
+		return true;
+	} else if (field[x][y-1].exists===true) {
+		return true;
+	}else if (field[x+1][y-1].exists===true) {
+		return true;
+	}else if (field[x+1][y].exists===true) {
+		return true;
+	}else if (field[x+1][y+1].exists===true) {
+		return true;
+	}else if (field[x][y+1].exists===true) {
+		return true;
+	}else if (field[x-1][y+1].exists===true) {
+		return true;
+	}else if (field[x-1][y].exists===true) {
+		return true;
+	} else {
+		return false;
+	}
 }
